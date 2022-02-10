7d954398a634ada2f2dba8c3fbdbc48c8bd45009
Marcin Nowak-Liebiediew <m.liebiediew@gmail.com>
2022-02-10 02:29:43 +0100

    feat: expand path to heading (fully qualified path)

--- org-anki.el
+++ org-anki.el
@@ -159,10 +159,35 @@ with result."
   latex-code
   )
 
+(defun get-keyword-key-value (kwd)
+     (let ((data (cadr kwd)))
+       (list (plist-get data :key)
+             (plist-get data :value))))
+
+(defun org-current-buffer-get-title ()
+      (nth 1
+       (assoc "TITLE"
+        (org-element-map (org-element-parse-buffer 'greater-element)
+            '(keyword)
+            #'get-keyword-key-value))))
+
+(defun org-anki-front-card-get-heading-path ()
+    (let (x '())
+        (push (org-entry-get nil "ITEM") x)
+        (org-element-map (org-element-lineage (org-element-at-point)) 'headline
+                        (lambda (hl) (push (org-element-property :raw-value hl) x)))
+        (push (org-current-buffer-get-title) x)
+        (string-join x " -> ")))
+
+(defun get-top-level-content ()
+    (nth 2 (nth 0 (org-element-map (org-element-parse-buffer) 'paragraph
+        (lambda (paragraph) (org-entry-get nil "ITEM") paragraph))))
+)
+
 (defun org-anki--note-at-point ()
   (let
       ((maybe-id (org-entry-get nil org-anki-prop-note-id))
-       (front (org-anki--string-to-html (org-entry-get nil "ITEM")))
+       (front (org-anki--string-to-html (org-anki-front-card-get-heading-path)))
        (back (org-anki--back-post-processing (org-anki--string-to-html (org-anki--entry-content-until-any-heading))))
        (tags (org-anki--get-tags))
        (deck (org-anki--find-prop org-anki-prop-deck org-anki-default-deck))
